e0f15fcb28a0b6cc69677c8edae9f6c0ea366520
EdissonGirald0 <edisson.giraldo@hotmail.com>
2022-10-30 15:05:39 -0500

    correccion de enrutado

--- cliente/components/Nav/Nav.tsx
+++ cliente/components/Nav/Nav.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-
+import { Link } from "react-router-dom";
 import styles from './Nav.scss';
-
+import Logo from '';
 export interface NavProps {
   prop?: string;
 }
@@ -9,3 +9,28 @@ export interface NavProps {
 export function Nav({prop = 'default value'}: NavProps) {
   return <div className={styles.Nav}>Nav {prop}</div>;
 }
+
+import Logo from './cssStilos/punk.jpg'
+
+
+
+function Nav({onSearch}) {
+  return (
+    <nav className="navbar nav">
+      <Link to='/'>
+        <span className="navbar-brand">
+          <img id="logo" src={Logo} width="30" height="30" className="d-inline-block align-top" alt="" />
+          Weather App
+        </span>
+      </Link>
+      <Link to='/about'>
+        <span>About</span>
+      </Link>
+        <SearchBar
+          onSearch={onSearch}
+        />
+    </nav>
+  );
+};
+
+export default Nav;
